669ea5e54245dcf68ee1acc2f33764fb85f0e2b6
auraShawn <51758471+auraShawn@users.noreply.github.com>
2020-01-31 18:49:45 -0500

    Pull ansible repo on first install/include cron

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+
+/usr/bin/ansible-pull -o -U https://github.com/auraShawn/ansible-workstation.git > /dev/null
