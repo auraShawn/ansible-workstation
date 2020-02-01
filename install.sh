902983519533b77b51adae7de4fc008a5b3a44c9
auraShawn <51758471+auraShawn@users.noreply.github.com>
2020-01-31 19:00:26 -0500

    added git install from apt

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+sudo apt install git
 
 /usr/bin/ansible-pull -o -U https://github.com/auraShawn/ansible-workstation.git > /dev/null
