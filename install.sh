0f6652ecb5abdaa5033f0630c8c50979f2b79769
Shawn DeFede <shawn@sdefe.de>
2022-07-31 14:49:05 -0400

    Added 1Password to Playbook

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt install software-properties-common
 # Removed for hirsute hippo as ansible is included in the default repos.
 # sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+pip3 install "ansible-lint"
 sudo apt install git
 
 /usr/bin/ansible-pull -o -U https://github.com/auraShawn/ansible-workstation.git > /dev/null
